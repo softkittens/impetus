b80feb5a6f739379e6617075f13084abc41a7337
Aleksandar <aleksandar@softkittens.com>
2025-10-25 00:13:09 +0200

    feat: add select2 and tooltip components with simplified script loading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 export * from "./runtime";
+
+// Auto-init support: <script type="module" src="./sparkle.js" defer init></script>
+if (typeof document !== 'undefined') {
+  const script = document.querySelector('script[type="module"][init]');
+  if (script) {
+    queueMicrotask(async () => {
+      const { init } = await import('./runtime');
+      init();
+    });
+  }
+}
